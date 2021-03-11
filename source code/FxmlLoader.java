b578644a66d4fb7458e4f85984336bb817c96c15
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2021-03-11 06:36:07 +0000

    Javadoc and bug fixes

--- source code/FxmlLoader.java
+++ source code/FxmlLoader.java
@@ -6,20 +6,24 @@ import javafx.scene.layout.Pane;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * This class is responsible for the functionality of switching between screens
+ */
 public class FxmlLoader {
-    private Pane view;
+    private Pane view;  //Represents a screen
 
+    /**
+     * Called to load a screen unto the user interface
+     * @param filename represents the name of file containing the graphic elements
+     * @return the loaded screen
+     */
     public Pane getPage(String filename){
         try {
             URL fileUrl = Main.class.getResource(filename);
-            view = new FXMLLoader().load(fileUrl);
+            view = FXMLLoader.load(fileUrl);
         }catch (IOException e) {
             e.printStackTrace();
         }
         return view;
     }
-
-
-
-
 }
