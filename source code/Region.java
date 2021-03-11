8476c45f1c97bf2e0d2de6c8e5eeaa04c2b80c14
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2021-03-11 06:45:28 +0000

    Javadoc and bug fixes

--- source code/Region.java
+++ source code/Region.java
@@ -2,42 +2,51 @@ package sample;
 
 import java.util.Stack;
 
-public class Region {
-    public static Rcc Oti = new Rcc("OTI");
-    public static Rcc BonoEast = new Rcc("BONO EAST");
-    public static Rcc Ahafo = new Rcc("AHAFO");
-    public static Rcc Bono = new Rcc("BONO");
-    public static Rcc NorthEast = new Rcc("NORTH EAST");
-    public static Rcc Savannah = new Rcc("SAVANNAH");
-    public static Rcc WesternNorth = new Rcc("WESTERN NORTH");
-    public static Rcc Western = new Rcc("WESTERN");
-    public static Rcc Volta = new Rcc("VOLTA");
-    public static Rcc GreaterAccra = new Rcc("GREATER ACCRA");
-    public static Rcc Eastern = new Rcc("EASTERN");
-    public static Rcc Ashanti = new Rcc("ASHANTI");
-    public static Rcc Central = new Rcc("CENTRAL");
-    public static Rcc Northern = new Rcc("NORTHERN");
-    public static Rcc UpperEast = new Rcc("UPPER EAST");
-    public static Rcc UpperWest = new Rcc("UPPER WEST");
+public class Region{
+    //List of all regions in Ghana
+    public static Rcc Oti = new Rcc("Oti Region");
+    public static Rcc BonoEast = new Rcc("Bono East Region");
+    public static Rcc Ahafo = new Rcc("Ahafo Region");
+    public static Rcc Bono = new Rcc("Bono Region");
+    public static Rcc NorthEast = new Rcc("North East Region");
+    public static Rcc Savannah = new Rcc("Savannah Region");
+    public static Rcc WesternNorth = new Rcc("Western North Region");
+    public static Rcc Western = new Rcc("Western Region");
+    public static Rcc Volta = new Rcc("Volta Region");
+    public static Rcc GreaterAccra = new Rcc("Greater Accra Region");
+    public static Rcc Eastern = new Rcc("Eastern Region");
+    public static Rcc Ashanti = new Rcc("Ashanti Region");
+    public static Rcc Central = new Rcc("Central Region");
+    public static Rcc Northern = new Rcc("Northern Region");
+    public static Rcc UpperEast = new Rcc("Upper East Region");
+    public static Rcc UpperWest = new Rcc("Upper West Region");
 
-    protected static class Rcc {
+    protected static class Rcc implements Collation{
+        //Represents the districts under a region
         protected Stack<District> districts = new Stack<>();
+
+        //Represents the name of the region
         private final String regionName;
 
+        //Constructor
+        /**
+         * @param regionName represents the name of the region
+         */
         Rcc(String regionName) {
             this.regionName = regionName;
             National.regions.add(this);
         }
 
-        public String getDistricts() {
-            return districts.toString();
-        }
-
+        /**
+         * @return the name of the region
+         */
         public String getRegionName() {
             return regionName;
         }
 
-
+        /**
+         * @return the total votes of NPP as an integer
+         */
         public int getNppResults() {
             int results = 0;
             for (District dcc : districts) {
@@ -45,7 +54,9 @@ public class Region {
             }
             return results;
         }
-
+        /**
+         * @return the total votes of NDC as an integer
+         */
         public int getNdcResults() {
             int results = 0;
             for (District dcc : districts) {
@@ -53,7 +64,9 @@ public class Region {
             }
             return results;
         }
-
+        /**
+         * @return the total votes of GUM as an integer
+         */
         public int getGumResults() {
             int results = 0;
             for (District dcc : districts) {
@@ -61,7 +74,9 @@ public class Region {
             }
             return results;
         }
-
+        /**
+         * @return the total votes of CPP as an integer
+         */
         public int getCppResults() {
             int results = 0;
             for (District dcc : districts) {
@@ -69,7 +84,9 @@ public class Region {
             }
             return results;
         }
-
+        /**
+         * @return the total votes of PPP as an integer
+         */
         public int getPppResults() {
             int results = 0;
             for (District dcc : districts) {
@@ -77,78 +94,15 @@ public class Region {
             }
             return results;
         }
-
-       /* public int getGcppResults() {
-            int results = 0;
-            for (District dcc : districts) {
-                results += dcc.getGcppResults();
-            }
-            return results;
-        }
-
+        /**
+         * @return the total votes of APC as an integer
+         */
         public int getApcResults() {
             int results = 0;
             for (District dcc : districts) {
                 results += dcc.getApcResults();
             }
             return results;
-        }
-
-        public int getLpgResults() {
-            int results = 0;
-            for (District dcc : districts) {
-                results += dcc.getLpgResults();
-            }
-            return results;
-        }
-
-        public int getPncResults() {
-            int results = 0;
-            for (District dcc : districts) {
-                results += dcc.getPncResults();
-            }
-            return results;
-        }
-
-        public int getPppResults() {
-            int results = 0;
-            for (District dcc : districts) {
-                results += dcc.getPppResults();
-            }
-            return results;
-        }
-
-        public int getNdpResults() {
-            int results = 0;
-            for (District dcc : districts) {
-                results += dcc.getNdpResults();
-            }
-            return results;
-        }
-
-        public int getIndpResults() {
-            int results = 0;
-            for (District dcc : districts) {
-                results += dcc.getIndpResults();
-            }
-            return results;
-        }*/
-
-        public String Summary() {
-            return  "\nTotal number of districts in the region: " + districts.size();
-                    /*"\n\n\t\t\t\t"+regionName+" REGIONAL RESULTS:\n\t\t\t\t\t" +
-                    PoliticalParties.NPP.name() + " : " + getNppResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.NDC.name() + " : " + getNdcResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.GUM.name() + " : " + getGumResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.CPP.name() + " : " + getCppResults() + " votes" + "\n\t\t\t\t\t" ;
-                    /*PoliticalParties.GFP.name() + " : " + getGfpResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.GCPP.name() + " : " + getGcppResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.APC.name() + " : " + getApcResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.LPG.name() + " : " + getLpgResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.PNC.name() + " : " + getPncResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.PPP.name() + " : " + getPppResults() + " votes" + "\n\t\t\t\t\t" +
-                    PoliticalParties.NDP.name() + " : " + getNdpResults() + " votes" + "\n\t\t\t" +
-                    PoliticalParties.INDEPENDENT.name() + " : " + getIndpResults() + " votes" + "\n";*/
         }
     }
 }
