9323037458b8ed3c601a2f23cf56aafa72f2984a
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2021-03-11 06:33:53 +0000

    Javadoc and bug fixes

--- source code/DashboardController.java
+++ source code/DashboardController.java
@@ -11,23 +11,34 @@ import javafx.scene.text.Text;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
+/**
+ * This class is responsible for the functionality of the home user interface or screen
+ */
 public class DashboardController implements Initializable {
-    @FXML private Text name;
-    @FXML private ImageView png;
-    @FXML private Circle status;
-    Image img = new Image(getClass().getResourceAsStream("/pics/Man.png"));
+    @FXML private Text name;       //Represents the name of the voter on the interface
+    @FXML private ImageView png;   //Represents an avatar of the voter's gender to add aesthetics to the interface
+    @FXML private Circle status;   //Represents a colour indication of whether or not a user is eligible to vote
+    Image img = new Image(getClass().getResourceAsStream("/pics/Man.png"));  //The image avatar
 
+    /**
+     * Called to initialize a controller after its root element has been completely processed.
+     * @param url represents the location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle represents the resources used to localize the root object, or null if the root object was not localized.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        name.setText(trackVoter().getName());
-        if (trackVoter().getGender().equals("Male")){ png.setImage(img); }
-        if (trackVoter().hasVoted()){
-            status.setFill(Paint.valueOf("red"));
+        name.setText(trackVoter().getName()); //sets the name on the screen to the logged in voter's name
+        if (trackVoter().getGender().equals("Male")){ png.setImage(img); } //sets avatar according to their gender
+        if (trackVoter().hasVoted()){              //sets the voting eligibility of the logged in voter(green default)
+            status.setFill(Paint.valueOf("red"));        //red if the voter has already voted
             status.setStroke(Paint.valueOf("red"));
         }
     }
 
+    /**
+     * Called to keep track of a voter that logs into the system
+     * @return an instance of Voter, the logged in voter
+     */
     public static Voter trackVoter(){
         for(Voter v: Admin.votersRegister){
             if (v.getVOTER_ID() == LoginController.voterID){ return v; }
