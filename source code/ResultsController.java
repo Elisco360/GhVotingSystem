3fde232efbd8f24a0606c402d5839ab1ba4c52ab
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2021-03-11 06:47:36 +0000

    Javadoc and bug fixes

--- source code/ResultsController.java
+++ source code/ResultsController.java
@@ -12,7 +12,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -21,29 +20,50 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ResultsController implements Initializable {
+    //Graphic element to allow selection of polling station
     @FXML private ComboBox<PollingStation> stations;
+
+    //Graphic element to display the corresponding district of the selected polling station
     @FXML private TextField districts;
+
+    //Graphic element to display the corresponding region of the selected polling station
     @FXML private TextField regions;
+
+    //Graphic element to indicate the eligibility of the user to vote
     @FXML private Circle status;
-    private PollingStation pollingStation;
+
+    //Sets the list of polling stations to be displayed on the user interface
     ObservableList<PollingStation> pollingStations = FXCollections.observableArrayList(Admin.pollingStations);
 
+    /**
+     * Called to initialize a controller after its root element has been completely processed.
+     * @param url represents the location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle represents the resources used to localize the root object, or null if the root object was not localized.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         if (DashboardController.trackVoter().hasVoted()){
-            status.setFill(Paint.valueOf(String.valueOf(Color.rgb(255,0,0))));
-            status.setStroke(Paint.valueOf("RED"));
+            status.setFill(Color.rgb(255,0,0));
+            status.setStroke(Color.rgb(255,0,0));
         }
         stations.setItems(pollingStations);
     }
 
+    /**
+     * Called to set the district and region fields to the corresponding polling station
+     */
     public void handleStations(){
-        pollingStation = stations.getValue();
+        PollingStation pollingStation = stations.getValue();
         districts.setText(pollingStation.getDistrict().getDistrictName());
         regions.setText(pollingStation.getDistrict().getRegion().getRegionName());
         DashboardController.trackVoter().setViewChoice(pollingStation);
     }
 
+    /**
+     * Called to display the result window
+     * @param e represents an action performed by the user(clicking)
+     * @throws IOException to catch any input and output errors
+     */
     public void handleViewResults(ActionEvent e) throws IOException {
         if(stations.getValue() != null){
             Stage stage = new Stage();
@@ -53,13 +73,17 @@ public class ResultsController implements Initializable {
             stage.resizableProperty().setValue(Boolean.FALSE);
             stage.show();
         }else {
+            //Prompt the user to select a polling station
             alert();
         }
 
     }
 
+    /**
+     * An alert to prompt the user to choose a polling station before they can access the results
+     */
     private void alert(){
-        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        Alert a = new Alert(Alert.AlertType.NONE);
         a.setTitle("Null Selection");
         a.setHeaderText("No Polling Station Selected");
         a.setContentText("Kindly select a polling station to enable access to results.");
