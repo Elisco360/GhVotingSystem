7df20bfbce26bafbd6a42242c0822429480ca53c
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2021-03-11 06:43:48 +0000

    Javadoc and bug fixes

--- source code/PollingStation.java
+++ source code/PollingStation.java
@@ -3,106 +3,106 @@ package sample;
 import java.io.*;
 import java.util.*;
 
-public class PollingStation {
-    protected ArrayList<Voter> voted = new ArrayList<>(); //Class variable
-    protected List<Integer> votes = new ArrayList<>(Collections.nCopies(5, 0));//Keep count of votes
-    protected  int[] pinkSheet = new int[5];
+/**
+ * This class is responsible for collating results of the respective political parties from the voters
+ */
+public class PollingStation implements Collation{
+    //Represents the name of the polling station
     private final String pollingStationName;
+
+    //Represents the district in which the polling station is situated
     private final District district;
+
+    //Represents a digital pink sheet or results sheet at the polling station level
     private final File results;
 
 
+    /**
+     * @param pollingStationName represents the name of the polling station
+     * @param district represents the district in which the polling station is situated
+     * @param results represents a digital pink sheet or results sheet at the polling station level
+     */
     PollingStation(String pollingStationName, District district, File results){
         this.pollingStationName = pollingStationName;
         this.district =  district;
         this.results = results;
         this.district.pollingStations.add(this);
-        try {
-            this.results.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        try { this.results.createNewFile(); }
+        catch (IOException e) { e.printStackTrace(); }
         readResults(this.results);
     }
 
+    /**
+     * @return the results obtained from the polling station
+     */
     public File getResults() {
         return results;
     }
 
+    /**
+     * Initialized votes of the respective political parties to zero
+     */
     public void setResults(){
         try {
             FileWriter writer = new FileWriter(this.results);
-            writer.write("0,0,0,0,0");
+            writer.write("0,0,0,0,0,0");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * @return the district of the polling station
+     */
     public District getDistrict() {
         return district;
     }
 
-    public String getPollingStationName() {
-        return pollingStationName;
-    }
-
+    /**
+     * @return the total votes of NPP as an integer
+     */
     public int getNppResults() {
-        readResults(this.results);
-        return pinkSheet[0];
+        return readResults(this.results)[0];
     }
+    /**
+     * @return the total votes of NDC as an integer
+     */
     public int getNdcResults() {
-        readResults(this.results);
-        return pinkSheet[1];
+        return readResults(this.results)[1];
     }
+    /**
+     * @return the total votes of GUM as an integer
+     */
     public int getGumResults() {
-        readResults(this.results);
-        return pinkSheet[2];
+        return readResults(this.results)[2];
     }
+    /**
+     * @return the total votes of CPP as an integer
+     */
     public int getCppResults() {
-        readResults(this.results);
-        return pinkSheet[3];
+        return readResults(this.results)[3];
     }
+    /**
+     * @return the total votes of PPP as an integer
+     */
     public int getPppResults() {
-        readResults(this.results);
-        return pinkSheet[4];
+        return readResults(this.results)[4];
     }
-   /* public int getGcppResults() {
-        PoliticalParties.GCPP.setNumberOfVotes(votes.get(5));
-        return PoliticalParties.GCPP.getNumberOfVotes();
-    }
+    /**
+     * @return the total votes of APC as an integer
+     */
     public int getApcResults() {
-        PoliticalParties.APC.setNumberOfVotes(votes.get(6));
-        return PoliticalParties.APC.getNumberOfVotes();
-    }
-    public int getLpgResults() {
-        PoliticalParties.LPG.setNumberOfVotes(votes.get(7));
-        return PoliticalParties.LPG.getNumberOfVotes();
-    }
-    public int getPncResults() {
-        PoliticalParties.PNC.setNumberOfVotes(votes.get(8));
-        return PoliticalParties.PNC.getNumberOfVotes();
-    }
-    public int getPppResults() {
-        PoliticalParties.PPP.setNumberOfVotes(votes.get(9));
-        return PoliticalParties.PPP.getNumberOfVotes();
-    }
-    public int getNdpResults() {
-        PoliticalParties.NDP.setNumberOfVotes(votes.get(10));
-        return PoliticalParties.NDP.getNumberOfVotes();
-    }
-    public int getIndpResults() {
-        PoliticalParties.INDEPENDENT.setNumberOfVotes(votes.get(11));
-        return PoliticalParties.INDEPENDENT.getNumberOfVotes();
-    }*/
-
-    @Override
-    public String toString(){
-        return pollingStationName;
+        return readResults(this.results)[5];
     }
 
-
-    public void readResults(File file){
+    /**
+     * Called to read the results on the results sheet
+     * @param file represents the results sheet of a particular polling station
+     * @return the results obtained from the results sheet as an integer array
+     */
+    public int[] readResults(File file){
+        int[] pinkSheet = new int[6];
         try{
             int i=0;
             Scanner reader = new Scanner(file);
@@ -111,10 +111,19 @@ public class PollingStation {
                     pinkSheet[i] = Integer.parseInt(s);
                     i+=1;
                 }
-            }else { setResults(); }
+            }
+            else { setResults(); }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return pinkSheet;
     }
 
+    /**
+     * @return name of polling station
+     */
+    @Override
+    public String toString() {
+        return pollingStationName;
+    }
 }
