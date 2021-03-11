7de33ee939c3dc272ed55e617aa862008e2d3c4e
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2021-03-11 07:19:01 +0000

    Javadoc and bug fixes

--- source code/VotedController.java
+++ source code/VotedController.java
@@ -10,17 +10,26 @@ import java.util.ResourceBundle;
 
 
 public class VotedController implements Initializable {
-    @FXML private ImageView nppImg;
-    @FXML private ImageView ndcImg;
-    @FXML private ImageView gumImg;
-    @FXML private ImageView cppImg;
-    @FXML private ImageView pppImg;
+    @FXML private ImageView nppImg;    //Represents a vote for NPP
+    @FXML private ImageView ndcImg;    //Represents a vote for NDC
+    @FXML private ImageView gumImg;    //Represents a vote for GUM
+    @FXML private ImageView cppImg;    //Represents a vote for CPP
+    @FXML private ImageView pppImg;    //Represents a vote for PPP
+    @FXML private ImageView apcImg;    //Represents a vote for APC
+
+    //Represents the party position who the voter has voted for
     private final int position = DashboardController.trackVoter().votedFor();
 
-    Image img = new Image(getClass().getResourceAsStream("/pics/fingerprint.png"));
+    Image img = new Image(getClass().getResourceAsStream("/pics/fingerprint.png")); //Thumb print image
 
+    /**
+     * Called to initialize a controller after its root element has been completely processed.
+     * @param url represents the location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle represents the resources used to localize the root object, or null if the root object was not localized.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Checks the party voted for and sets its image to a the thumb print
         if(DashboardController.trackVoter().hasVoted()){
             switch (position){
                 case 1:
@@ -37,6 +46,9 @@ public class VotedController implements Initializable {
                     break;
                 case 5:
                     pppImg.setImage(img);
+                    break;
+                case 6:
+                    apcImg.setImage(img);
                     break;
                 default:
                     break;
