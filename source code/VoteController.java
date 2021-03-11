6bf2549fd2553407305ccb590542cc05bbed915f
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2021-03-11 06:52:44 +0000

    Javadoc and bug fixes

--- source code/VoteController.java
+++ source code/VoteController.java
@@ -14,24 +14,45 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * This class is responsible for providing functionality of the voter interface
+ */
 public class VoteController implements Initializable {
-    @FXML private RadioButton npp;
-    @FXML private RadioButton ndc;
-    @FXML private RadioButton gum;
-    @FXML private RadioButton cpp;
-    @FXML private RadioButton ppp;
-    @FXML private Button submit;
-    @FXML private ImageView nppImg;
-    @FXML private ImageView ndcImg;
-    @FXML private ImageView gumImg;
-    @FXML private ImageView cppImg;
-    @FXML private ImageView pppImg;
-    Image img = new Image(getClass().getResourceAsStream("/pics/fingerprint.png"));
-    private final File votedList = new File("voted.txt");
+    @FXML private RadioButton npp;    //Represents selection choice for NPP
+    @FXML private RadioButton ndc;    //Represents selection choice for NDC
+    @FXML private RadioButton gum;    //Represents selection choice for GUM
+    @FXML private RadioButton cpp;    //Represents selection choice for CPP
+    @FXML private RadioButton ppp;    //Represents selection choice for PPP
+    @FXML private RadioButton apc;    //Represents selection choice for APC
+    @FXML private Button submit;      //Represents button for choice approval
+    @FXML private ImageView nppImg;   //Represents a vote for NPP
+    @FXML private ImageView ndcImg;   //Represents a vote for NDC
+    @FXML private ImageView gumImg;   //Represents a vote for GUM
+    @FXML private ImageView cppImg;   //Represents a vote for CPP
+    @FXML private ImageView pppImg;   //Represents a vote for PPP
+    @FXML private ImageView apcImg;   //Represents a vote for APC
+    Image img = new Image(getClass().getResourceAsStream("/pics/fingerprint.png"));  //Thumb print image
+    private final File votedList = new File("voted.txt");   //List of voters who have voted
 
+    /**
+     * Called to initialize a controller after its root element has been completely processed.
+     * @param url represents the location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle represents the resources used to localize the root object, or null if the root object was not localized.
+     */
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {}
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            votedList.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * Called to provided the functionality that shows that a voter has voted, thus, updating results and adding the
+     * voter to the voted list. It also handles the user interface response when a user casts their vote
+     * @param e represents an action performed by the user (Clicking)
+     */
     public void trackVote(ActionEvent e) {
         if(npp.isSelected()){
             DashboardController.trackVoter().castVote(1);
@@ -42,6 +63,7 @@ public class VoteController implements Initializable {
             gum.setVisible(false);
             cpp.setVisible(false);
             ppp.setVisible(false);
+            apc.setVisible(false);
             submit.setVisible(false);
             nppImg.setImage(img);
         }
@@ -54,6 +76,7 @@ public class VoteController implements Initializable {
             gum.setVisible(false);
             cpp.setVisible(false);
             ppp.setVisible(false);
+            apc.setVisible(false);
             submit.setVisible(false);
             ndcImg.setImage(img);
         }
@@ -66,6 +89,7 @@ public class VoteController implements Initializable {
             gum.setVisible(false);
             cpp.setVisible(false);
             ppp.setVisible(false);
+            apc.setVisible(false);
             submit.setVisible(false);
             gumImg.setImage(img);
         }
@@ -78,9 +102,11 @@ public class VoteController implements Initializable {
             gum.setVisible(false);
             cpp.setVisible(false);
             ppp.setVisible(false);
+            apc.setVisible(false);
             submit.setVisible(false);
             cppImg.setImage(img);
-        }else if(ppp.isSelected()){
+        }
+        else if(ppp.isSelected()){
             DashboardController.trackVoter().castVote(5);
             recordVoter();
             updateVotes(DashboardController.trackVoter().getPollingStation().getResults());
@@ -89,30 +115,49 @@ public class VoteController implements Initializable {
             gum.setVisible(false);
             cpp.setVisible(false);
             ppp.setVisible(false);
+            apc.setVisible(false);
             submit.setVisible(false);
             pppImg.setImage(img);
         }
+        else if(apc.isSelected()){
+            DashboardController.trackVoter().castVote(6);
+            recordVoter();
+            updateVotes(DashboardController.trackVoter().getPollingStation().getResults());
+            npp.setVisible(false);
+            ndc.setVisible(false);
+            gum.setVisible(false);
+            cpp.setVisible(false);
+            ppp.setVisible(false);
+            apc.setVisible(false);
+            submit.setVisible(false);
+            apcImg.setImage(img);
+        }
     }
 
-
-
+    /**
+     * This method writes into the voted list the id and candidate who was voted for by a voter
+     */
     public void recordVoter(){
         try {
             FileWriter writer = new FileWriter(votedList, true);
-            for (Voter v: DashboardController.trackVoter().getPollingStation().voted) {
-                writer.write(v.getVOTER_ID()+","+DashboardController.trackVoter().getCandidate()+"\n");
-            }
+            Voter currentVoter = DashboardController.trackVoter();
+            writer.write(currentVoter.getVOTER_ID()+","+currentVoter.getCandidate()+"\n");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Called to read the results sheet and add one vote for the candidate who has been voted
+     * @param file represents the particular results sheet to be updated
+     */
     public void updateVotes(File file){
         try{
             FileWriter writer = new FileWriter(file, false);
-            int[] votes = DashboardController.trackVoter().getPollingStation().pinkSheet;
-            String[] values = new String[5];
+            Voter currentVoter = DashboardController.trackVoter();
+            int[] votes = currentVoter.getPollingStation().readResults(currentVoter.getPollingStation().getResults());
+            String[] values = new String[6];
             switch (DashboardController.trackVoter().votedFor()){
                 case 1:
                     votes[0] = votes[0]+1;
@@ -128,6 +173,9 @@ public class VoteController implements Initializable {
                     break;
                 case 5:
                     votes[4] = votes[4]+1;
+                    break;
+                case 6:
+                    votes[5] = votes[5]+1;
                     break;
                 default:
                     break;
